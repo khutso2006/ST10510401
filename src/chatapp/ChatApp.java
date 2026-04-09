f4bc001b5fc0a4094aa8bd5d079b0c0402866de5
khutso2006 <khutsojesley@gmail.com>
2026-04-09 21:26:57 +0200

    first commit

--- src/chatapp/ChatApp.java
+++ src/chatapp/ChatApp.java
@@ -10,56 +10,94 @@ public class ChatApp {
        //declaring variables
        String firstname;
        String lastname;
-       String Username;
-       String Password;
+       String username;
+       String password;
        String cellphoneNumber;
        //Scanner
+     
      Scanner scn = new Scanner(System.in);
-             
-      } 
-    //creating an account  
-    public static void register(Scanner scn) {
         //class1 + Class2=Linking classes
-         Login login = new Login();           
+        Login login = new Login();
+        System.out.println( "(REGISTER)");
+        
        System.out.println("please enter firstName");
-       String firstname = scn.next();
+       firstname = scn.nextLine();
        
        System.out.println("please enter lastName");
-       String lastname = scn.next();
+       lastname = scn.nextLine();
        
        System.out.println("Enter your username");
-       String Username = scn.next();
+       username = scn.nextLine();
        
-       if(login.checkUserName(Username)){
+       if(login.checkUserName(username)){
            System.out.println("Username is succefully captured");
        }else{
            System.out.println("UserName is not correctly formatted, please ensure that user name" 
               + "contains an under score and no more than five characters in length ");
+           
+
        }
        
-        System.out.println("please enter passWord");
-       String Password = scn.nextLine();
        
-       if(login.checkPassword(Password)){
+        System.out.println("please enter passWord");
+       password = scn.nextLine();
+       
+       if(login.checkPassword(password)){
            System.out.println("password is succefully captured");
        }else{
            System.out.println("Password is not correctly formated,ensure that password contains at least 8 characteres "
                    +"a capital letter,number and special character");
           
+
        }
        
         System.out.println("please enter cellphone number");
-       String cellphoneNumber = scn.nextLine();
+       cellphoneNumber = scn.nextLine();
        
        if(login.checkCellphone(cellphoneNumber)){
            System.out.println("cellphone number is succefully added");
+           
        }else{
           System.out.println("cellphone number is incorrectly formatted or does not contain international code ");
-          
-    }  
-     
+           return;  
+       }
+     //registration confirmation
+     {
+      System.out.println(login.checkregisterUser(username ,password));
+       
+      System.out.println("LOGIN");
+      
+         System.out.println("Enter username");
+         String enteredUsername = scn.nextLine();
+        
+         System.out.println("Enter password");
+         String enteredPassword = scn.nextLine();
+        
+        boolean loginUser = login.loginUser( password ,username, enteredUsername,enteredPassword);
+        
+              if(loginUser){
+                  System.out.println("Login successful!");
+                }else{
+                   System.out.println("Login failed"); 
+                  
+                  
+                  
+              
     }
-           }
+    }   
+       
+    }      
+   
+}     
+      
+          
+       
+
+        
+             
+
+
+
          
        
 
--- src/chatapp/Login.java
+++ src/chatapp/Login.java
@@ -1,6 +1,6 @@
 package chatapp;
 
-  import java.util.Scanner;
+ 
 
 
 /**
@@ -8,59 +8,90 @@ package chatapp;
  * @author KHUTSO
  */
 public class Login {
-
+     
         private String enteredfirstName;
         private String enteredlastName;
-        private String enteredUsername;
-        private String enteredpassWord;
+        private String enteredusername;
+        private String enteredpassword;
         private String enteredcellphoneNumber;
        
         
       boolean isLoggedIn = false;
+    private final String firstName;
+    private final String lastName;
+    private final String username;
+    private final String password;
+    private final String cellphoneNumber;
        
     //userName validation
     public boolean checkUserName(String enteredUsername){
     
             //password validation
-            return enteredUsername.contains("_") && enteredUsername.length() <=5;
-     
-    }
-    
-  public boolean checkPassword(String enteredPassword){ 
-      String passwordValidation = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
-      
-            return enteredPassword.matches(passwordValidation);   
-  }  
-   //cellphone validation
+            return(enteredUsername.contains("_") && enteredUsername.length() <=5);
+            
    
+    }
+  public boolean checkPassword(String enteredPassword){ 
+      String passwordValidation =( "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$");
+      
   
+   return enteredPassword.matches(passwordValidation);   
+  
+   //cellphone validation
+  }
+     
+     
     public boolean checkCellphone(String enteredCellphoneNumber){
        String cellphoneValidation = "^(\\+27)[6-8][0-9]{1}[- ]?[0-9]{3}[- ]?[0-9]{4}$"; 
        
             return enteredCellphoneNumber.matches(cellphoneValidation);
         }
-    public String checkregisterUser(String enteredUsername,String enteredpassword){
-       
+    
+     
+       public String  checkregisterUser(String enteredUsername ,String enteredpassword){
       if(!checkUserName(enteredUsername)){
-          return "Username is incorrectly formated";
-      }
-      if(!checkPassword(enteredpassword)){    
-          return "password does not meet complexity"; 
-   
-      }
-      return "the two above conditions have been met, and the user has been registered successfully. ";
-    }
-
-    public Login(){
-        this.enteredfirstName = enteredfirstName;
-        this.enteredlastName = enteredlastName;
-        this.enteredUsername = enteredUsername;
-        this.enteredpassWord = enteredpassWord;
-        this.enteredcellphoneNumber = enteredcellphoneNumber;
+            System.out.println( "Username is incorrectly formated");
         
+      }
+       if(!checkPassword(enteredpassword)){   
+             System.out.println("password does not meet complexity"); 
+          
+       }
+       else{
+             System.out.println ("the two above conditions have been met,and the user have been registered successfuly");
+       }
+      
+       return"";
+       }
        
+            
+  public boolean loginUser(String username, String password,String enteredUsername,String enteredPassword) {
+        // Compare entered details with stored ones
+        if (username.equals(enteredUsername) && password.equals(enteredPassword)){
+        return true;
+      }else{
+       return false;
     }
-   
-}    
- 
-  
+  }
+    // METHOD: Return login status message
+    public String returnLoginStatus(boolean loginSuccess) {
+
+        if (loginSuccess) {
+            return "Welcome " + firstName + " " + lastName + ", it is great to see you again!";
+        } else {
+            return "Username or password incorrect, please try again.";
+        
+    }
+}  
+         
+   //constractor 
+   public Login(){
+        this.firstName = enteredfirstName;
+        this.lastName = enteredlastName;
+        this.username = enteredusername;
+        this.password = enteredpassword;
+        this.cellphoneNumber = enteredcellphoneNumber;
+   }
+}
+
+
