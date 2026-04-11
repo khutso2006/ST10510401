5abbaed4d34bdf59244802efae148c50f763ce0a
khutso2006 <khutsojesley@gmail.com>
2026-04-11 11:57:57 +0200

    second commit

--- test/chatapp/ChatAppTest.java
+++ test/chatapp/ChatAppTest.java
@@ -1,37 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package chatapp;
 
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author KHUTSO
- */
+
 public class ChatAppTest {
   
     public ChatAppTest() {
     }
-   
-   
-    @Test
-    public void testUserName(){
+ @Test
+    public void testUserNamevalid(){
         Login log = new Login();
         boolean actual = log.checkUserName("Kyl_1");
         boolean expected = true;        
-        assertEquals("Expect to be true",expected, actual ); 
+        assertEquals("Welcome <user first name> <user last name> it is great to see you.",expected, actual ); 
         
        
     }
+    
+     @Test
+    public void testUserNameinvaild(){
+        Login log = new Login();
+        boolean actual = log.checkUserName("Kyl!!!!!");
+        boolean expected = false;        
+        assertEquals("Password is not correctly formatted; please ensure that the password"
+                + " contains at least eight characters, a capital letter, a number, and a special character.",expected, actual ); 
+        
+    
+    }
+    
    @Test
     public void testpassword(){
         Login log = new Login();
         boolean actual = log.checkPasswordREGEX("Ch&&sec@ke99!");
         boolean expected = true;        
+        assertEquals("Expect to be true",expected, actual );  
+        
+    }
+        @Test
+    public void testpasswordinvalid(){
+        Login log = new Login();
+        boolean actual = log.checkPasswordREGEX("password");
+        boolean expected = false;        
         assertEquals("Expect to be true",expected, actual );  
 }
  @Test
@@ -41,4 +52,13 @@ public class ChatAppTest {
         boolean expected = true;        
         assertEquals("Expect to be true",expected, actual );    
 }
-}
+ @Test   
+ public void testcellphonenumberinvaild(){
+        Login log = new Login();
+        boolean actual = log.CellphoneNumberIsValidREGEX("08966553");
+        boolean expected = false;        
+        assertEquals("Expect to be true",expected, actual );       
+    
+    
+ }
+    }
